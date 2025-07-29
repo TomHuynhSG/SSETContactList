40f8477743516dcf2cf308d9ecbb890d81425d24
TomHuynhSG <tomhuynhsg@gmail.com>
2025-07-29 10:59:50 +0700

    Add an example to use Json URL instead of file

--- SSETContactList/Model/ModelData.swift
+++ SSETContactList/Model/ModelData.swift
@@ -6,15 +6,14 @@
 //
 
 import Foundation
-import MapKit
 
+var jsonURL = "https://www.jsonkeeper.com/b/EBBME"
 
-var contacts = decodeJsonFromJsonFile(jsonFileName: "contacts.json")
+var contacts = decodeJsonFromJsonFile(inputJsonURL: jsonURL)
 
-// How to decode a json file into a struct
-func decodeJsonFromJsonFile(jsonFileName: String) -> [Contact] {
-    if let file = Bundle.main.url(forResource: jsonFileName, withExtension: nil){
-        if let data = try? Data(contentsOf: file) {
+func decodeJsonFromJsonFile(inputJsonURL: String) -> [Contact] {
+    if let url = URL(string: inputJsonURL) {
+        if let data = try? Data(contentsOf: url) {
             do {
                 let decoder = JSONDecoder()
                 let decoded = try decoder.decode([Contact].self, from: data)
@@ -23,8 +22,6 @@ func decodeJsonFromJsonFile(jsonFileName: String) -> [Contact] {
                 fatalError("Failed to decode JSON: \(error)")
             }
         }
-    } else {
-        fatalError("Couldn't load \(jsonFileName) file")
     }
     return [ ] as [Contact]
 }
